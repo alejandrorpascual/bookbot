5519e7937c6186b63368d6158e175f444f7bc196
alejandrorpascual <alexandros2707@gmail.com>
2025-04-27 11:59:30 -0400

    use command args

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from stats import execute_main
+import sys
 
 
 def main():
-    execute_main("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    execute_main(file_path)
 
 
 main()
